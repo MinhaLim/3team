10a6754432bbcca3f15d77ef2eec64b645015c42
minha <minha@userA>
2022-12-28 16:21:47 +0900

    20221228

---
+++ sampleshop/src/main/java/com/sampleshop/SampleshopApplication.java
@@ -0,0 +1,13 @@
+package com.sampleshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleshopApplication.class, args);
+	}
+
+}
---
+++ sampleshop/src/test/java/com/sampleshop/SampleshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampleshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
